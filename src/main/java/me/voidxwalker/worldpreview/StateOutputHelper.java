a1422da0b07063cf93d2c0010b7b165bc4a14508
pixfumy <95588510+pixfumy@users.noreply.github.com>
2023-04-04 23:59:47 -0400

    java 8 compatibility

--- src/main/java/me/voidxwalker/worldpreview/StateOutputHelper.java
+++ src/main/java/me/voidxwalker/worldpreview/StateOutputHelper.java
@@ -3,8 +3,10 @@ package me.voidxwalker.worldpreview;
 import org.apache.logging.log4j.Level;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -12,7 +14,7 @@ import java.util.concurrent.Executors;
  * A helper class for outputting and logging the current state of resetting to help with macros and verification without interrupting the regular flow of the game.
  */
 public final class StateOutputHelper {
-    private static final Path OUT_PATH = Path.of("wpstateout.txt");
+    private static final Path OUT_PATH = Paths.get("wpstateout.txt");
     private static final ExecutorService EXECUTOR = Executors.newSingleThreadExecutor();
     // Storage variable, not necessarily involved or required for using outputState()
     public static int loadingProgress = 0;
@@ -43,7 +45,7 @@ public final class StateOutputHelper {
     private static void outputStateInternal(String string) {
         try {
             // Java 11+ method
-            Files.writeString(OUT_PATH, string);
+            Files.write(OUT_PATH, string.getBytes(StandardCharsets.UTF_8));
             WorldPreview.log(Level.INFO, "WorldPreview State: " + string);
         } catch (IOException ignored) {
             WorldPreview.log(Level.ERROR, "Failed to write state output!");
